a9f659807bf4774f9131db54eac90e8ca2016727
Ryan Grenz <ryan.grenz@digital.hmrc.gov.uk>
2015-06-17 09:52:27 +0100

    Initial import

---
+++ webpack.dev.config.js
@@ -0,0 +1,23 @@
+'use strict';
+
+var config = require('./webpack.base.config.js');
+
+config.devServer = {
+  contentBase: 'src',
+  stats: {
+    colors: true
+  },
+  historyApiFallback: true
+};
+
+config.module.loaders = config.module.loaders.concat([
+  {test: /\.js(x)?$/, loaders: [
+    'react-hot',
+    'babel',
+    'flowcheck',
+    'babel?blacklist=flow&optional=runtime'],
+    exclude: /node_modules/},
+  {test: /\.css$/, loader: 'style!css'}
+]);
+
+module.exports = config;
