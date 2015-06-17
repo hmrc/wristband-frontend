a2fc342035d0312e0041a44feedcabe0656d9b0b
Ryan Grenz <ryan.grenz@digital.hmrc.gov.uk>
2015-06-17 17:16:43 +0100

    [WEBOPS-2065] Add semantic-ui framework for rapid UI development

--- src/js/app.jsx
+++ src/js/app.jsx
@@ -1,6 +1,10 @@
 import React from 'react/addons';
 import { RouteHandler } from 'react-router';
 
+import '../vendor/semantic/dist/semantic.css';
+import '../vendor/semantic/dist/semantic.js';
+
+
 export default class App extends React.Component {
   render() {
     return (
