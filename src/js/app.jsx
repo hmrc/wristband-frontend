81ced10d2f93c122c3100be126e1d1a386d69315
Ryan Grenz <ryan.grenz@digital.hmrc.gov.uk>
2015-06-23 15:44:07 +0100

    Updated React code and switched css to scss

--- src/js/app.jsx
+++ src/js/app.jsx
@@ -1,10 +1,79 @@
+/* @flow */
 import React from 'react/addons';
 import { RouteHandler } from 'react-router';
+import {
+  Grid,
+  Unit,
+  Column,
+  Dropdown,
+} from 'react-semantify';
+
+import '../vendor/semantic/dist/semantic.css';
+import '../vendor/semantic/dist/semantic.js';
+
+var $ = window.jQuery;
 
 export default class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      app: '',
+      config: {}
+    };
+  }
+
+  componentWillMount() {
+    this.fetchData();
+  }
+
+  componentWillReceiveProps() {
+    this.fetchData();
+  }
+
+  componentDidMount() {
+    var self = this;
+    $('.dropdown').dropdown({
+      onChange: function (value, text, $selectedItem) {
+        self.setState({app: value});
+      }
+    });
+  }
+
+  fetchData() {
+    $.getJSON('/wristband-api/config', (config) => {
+      this.setState({config: config});
+    });
+  }
+
   render() {
+    var options = '';
+
+    if (this.state.config.hasOwnProperty('apps')) {
+      options = this.state.config.apps.map(function(app, index) {
+        return (<option key={index} value={app}>{app}</option>);
+      });
+    }
+
     return (
-      <div>Hi there!</div>
+      <div className="ui grid">
+        <div className="twelve wide column row">
+          <div className="twelve wide column row">
+            <h1>Promotions</h1>
+          </div>
+
+          <div className="ui form twelve wide column row">
+            <div className="field">
+              <label>Service</label>
+              <select className="ui search dropdown">
+                { options }
+              </select>
+            </div>
+          </div>
+
+
+        </div>
+      </div>
     );
   }
 }
--- src/js/routes.jsx
+++ src/js/routes.jsx
@@ -5,5 +5,5 @@ const { DefaultRoute, Route, NotFoundRoute } = Router;
 import App from './app.jsx';
 
 export default (
-    <Route path="/" handler={App}/>
+  <Route path="/" handler={App}/>
 );
