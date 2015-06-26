4100b30603ae5029a5a4f8aef8e86ac638b1bc57
Ryan Grenz <ryan.grenz@digital.hmrc.gov.uk>
2015-06-26 17:01:16 +0100

    Added filterable application lists using FixedDataTable

--- src/js/app.jsx
+++ src/js/app.jsx
@@ -1,19 +1,27 @@
 /* @flow */
-import React from 'react/addons';
-import { RouteHandler } from 'react-router';
+import React from 'react';
+import RouteHandler from 'react-router';
+import FixedDataTable from 'fixed-data-table';
 
+import '../../node_modules/fixed-data-table/dist/fixed-data-table.css';
 import '../vendor/semantic/dist/semantic.css';
+import '../scss/styles.css';
 import '../vendor/semantic/dist/semantic.js';
 
 var $ = window.jQuery;
+
+var Table = FixedDataTable.Table;
+var Column = FixedDataTable.Column;
 
 export default class App extends React.Component {
   constructor() {
     super();
 
     this.state = {
-      app: '',
-      config: {}
+      config: [],
+      filteredRows: [],
+      filterBy: '',
+      middleColWidth: 0
     };
   }
 
@@ -25,48 +33,109 @@ export default class App extends React.Component {
     this.fetchData();
   }
 
+  setMiddleColWidth() {
+    this.state.middleColWidth = React.findDOMNode(this.refs.middleCol).offsetWidth;
+    this.forceUpdate();
+  }
+
   componentDidMount() {
-    var self = this;
-    $('.dropdown').dropdown({
-      onChange: function (value, text, $selectedItem) {
-        self.setState({app: value});
-      }
-    });
+    this.setMiddleColWidth();
+    window.addEventListener('resize', this.setMiddleColWidth.bind(this));
   }
 
   fetchData() {
     $.getJSON('/wristband-api/config', (config) => {
-      this.setState({config: config});
+      this.setState({
+        config: config,
+        filteredRows: config.apps
+      });
     });
   }
 
-  render() {
-    var options = '';
+  rowGetter(rowIndex) {
+    var appConfig = this.state.filteredRows;
+    return {
+      appName: appConfig[rowIndex].name,
+      qaVersion: appConfig[rowIndex].qa.version,
+      stagingVersion: appConfig[rowIndex].staging.version
+    };
+  }
 
-    if (this.state.config.hasOwnProperty('apps')) {
-      options = this.state.config.apps.map(function(app, index) {
-        return (<option key={index} value={app}>{app}</option>);
-      });
-    }
+  _filterRowsBy(filterBy) {
+    var rows = this.state.config.apps.slice();
+    var filteredRows = filterBy ? rows.filter(function(row){
+      return row.name.toLowerCase().indexOf(filterBy.toLowerCase()) >= 0;
+    }) : rows;
+
+    this.setState({
+      filteredRows: filteredRows,
+      filterBy: filterBy
+    });
+  }
+
+  _onFilterChange(e) {
+    this._filterRowsBy(e.target.value);
+  }
+
+  promoteApp(e) {
+    e.preventDefault();
+    // TODO: AJAX req
+  }
+
+  deployButtonRenderer() {
+    return (
+      <button className="ui red button" onClick={this.promoteApp}>
+        Deploy <i className="chevron right icon"></i>
+      </button>
+    );
+  }
+
+  render() {
+    var apps = (this.state.config.hasOwnProperty('apps')) ?
+      this.state.config.apps : [];
 
     return (
-      <div className="ui grid">
-        <div className="twelve wide column row">
-          <div className="twelve wide column row">
-            <h1>Promotions</h1>
-          </div>
-
-          <div className="ui form twelve wide column row">
-            <div className="field">
-              <label>Service</label>
-              <select className="ui search dropdown">
-                { options }
-              </select>
+      <div className="ui centered grid">
+        <div className="one wide column"></div>
+        <div ref="middleCol" className="ui ten wide column">
+          <div className="ui icon input">
+            <input type="text" onChange={this._onFilterChange.bind(this)} placeholder='Filter Services..'/>
+              <i className="search icon"></i>
             </div>
-          </div>
-
-
+          <Table
+            rowHeight={50}
+            rowGetter={this.rowGetter.bind(this)}
+            rowsCount={this.state.filteredRows.length}
+            width={this.state.middleColWidth}
+            height={500}
+            headerHeight={50}>
+            <Column
+              label="App"
+              width={100}
+              flexGrow={1}
+              dataKey="appName"
+              />
+            <Column
+              label="QA"
+              width={100}
+              dataKey="qaVersion"
+              align="center"
+              />
+            <Column
+              cellRenderer={this.deployButtonRenderer.bind(this)}
+              width={120}
+              dataKey="deployButton"
+              align="center"
+              />
+            <Column
+              label="Staging"
+              width={100}
+              dataKey="stagingVersion"
+              align="center"
+              />
+          </Table>
         </div>
+        <div className="one wide column"></div>
       </div>
     );
   }
