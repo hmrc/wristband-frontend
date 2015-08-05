2a7ce202e47be501c5f2f5ac2272bf0f2b3445f6
Tom van Neerijnen <tom@tomvn.com>
2015-08-05 12:26:59 +0100

    WEBOPS-2264: remove not working find/replace on prod index.html

--- webpack.config.js
+++ webpack.config.js
@@ -18,17 +18,8 @@ config.plugins = config.plugins.concat(
         this.plugin('done', function(stats) {
           var fs = require('graceful-fs');
 
-          fs.readFile('./src/index.html', 'utf8', function(readErr, data) {
+          fs.readFile('./public/index.html', 'utf8', function(readErr, data) {
             if (readErr) return console.log(readErr);
-
-            data = data.replace(
-                '</head>',
-                '<link rel="stylesheet" href="/js/style.css"/></head>'
-            );
-
-            if (production) {
-              data = config.revFiles(data, stats.toJson().assets);
-            }
 
             fs.writeFile('./dist/index.html', data, 'utf8', function(writeErr) {
               if (writeErr) return console.log(writeErr);
