bcafd512e36e43ecccc7b0c2634aabd88a24e06a
Rory Powis <rory.powis@digital.hmrc.gov.uk>
2015-08-06 15:49:26 +0100

    [WEBOPS-2264] Update output paths
    Output built assets to /dist/build and update all references to use that
    path.

--- webpack.config.js
+++ webpack.config.js
@@ -11,32 +11,31 @@ config.module.loaders = config.module.loaders.concat([
   {test: /\.css$/, loader: ExtractTextPlugin.extract('style', 'css')}
 ]);
 
-config.plugins = config.plugins.concat(
-    [
-      new ExtractTextPlugin(config.output.css),
-      function() {
-        this.plugin('done', function(stats) {
-          var fs = require('graceful-fs');
+config.plugins = config.plugins.concat([
+  new ExtractTextPlugin(config.output.css),
+  function() {
+    this.plugin('done', function(stats) {
+      var fs = require('graceful-fs');
 
-          fs.readFile('./src/index.html', 'utf8', function(readErr, data) {
-            if (readErr) return console.log(readErr);
+      fs.readFile('./public/index.html', 'utf8', function(readErr, data) {
+        if (readErr) return console.log(readErr);
 
-            data = data.replace(
-                '</head>',
-                '<link rel="stylesheet" href="/js/style.css"/></head>'
-            );
+        data = data.replace(
+            '</head>',
+            '<link rel="stylesheet" href="/build/style.css"/></head>'
+        );
 
-            fs.writeFile('./dist/index.html', data, 'utf8', function(writeErr) {
-              if (writeErr) return console.log(writeErr);
-            });
-          });
-          fs.mkdir('./dist/ping',function(e){
-            fs.writeFile('./dist/ping/ping', '', 'utf8', function(writeErr) {
-              if (writeErr) return console.log(writeErr);
-            });
-          });
+        fs.writeFile('./dist/index.html', data, 'utf8', function(writeErr) {
+          if (writeErr) return console.log(writeErr);
         });
-      }
-    ]);
+      });
+      fs.mkdir('./dist/ping',function(e){
+        fs.writeFile('./dist/ping/ping', '', 'utf8', function(writeErr) {
+          if (writeErr) return console.log(writeErr);
+        });
+      });
+    });
+  }
+]);
 
 module.exports = config;
