e26b6d05dc85222a4a13747e0df71fe41fea27e1
Rory Powis <rory.powis@digital.hmrc.gov.uk>
2015-08-06 14:00:00 +0100

    [WEBOPS-2264] Remove prod specific config as it's breaking the build
    Render react app into div instead of body. This is to avoid clobbering the
    entire contents of the body tag, including script tags.
    
    Remove postcss webpack loader
    
    Output built assets to /dist/build and update all references to use that
    path.

--- src/entry.jsx
+++ src/entry.jsx
@@ -3,5 +3,5 @@ import Router from 'react-router';
 import routes from './js/routes.jsx';
 
 Router.run(routes, Router.HistoryLocation, (Handler, state) => {
-  React.render(<Handler/>, document.body);
+  React.render(<Handler/>, document.getElementById('app'));
 });
--- webpack.config.js
+++ webpack.config.js
@@ -4,43 +4,38 @@ var ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 var production = process.env.NODE_ENV === 'production';
 var env = production ? 'production' : 'development';
-var config = production ? require('./webpack.prod.config.js') : require('./webpack.base.config.js');
+var config = require('./webpack.base.config.js');
 
 config.module.loaders = config.module.loaders.concat([
   {test: /\.jsx?$/, loader: 'babel?optional=runtime', exclude: /node_modules/},
-  {test: /\.css$/, loader: ExtractTextPlugin.extract('style', 'css?importLoaders=1!postcss')}
+  {test: /\.css$/, loader: ExtractTextPlugin.extract('style', 'css')}
 ]);
 
-config.plugins = config.plugins.concat(
-    [
-      new ExtractTextPlugin(config.output.css),
-      function() {
-        this.plugin('done', function(stats) {
-          var fs = require('graceful-fs');
+config.plugins = config.plugins.concat([
+  new ExtractTextPlugin(config.output.css),
+  function() {
+    this.plugin('done', function(stats) {
+      var fs = require('graceful-fs');
 
-          fs.readFile('./src/index.html', 'utf8', function(readErr, data) {
-            if (readErr) return console.log(readErr);
+      fs.readFile('./public/index.html', 'utf8', function(readErr, data) {
+        if (readErr) return console.log(readErr);
 
-            data = data.replace(
-                '</head>',
-                '<link rel="stylesheet" href="/js/style.css"/></head>'
-            );
+        data = data.replace(
+            '</head>',
+            '<link rel="stylesheet" href="/build/style.css"/></head>'
+        );
 
-            if (production) {
-              data = config.revFiles(data, stats.toJson().assets);
-            }
-
-            fs.writeFile('./dist/index.html', data, 'utf8', function(writeErr) {
-              if (writeErr) return console.log(writeErr);
-            });
-          });
-          fs.mkdir('./dist/ping',function(e){
-            fs.writeFile('./dist/ping/ping', '', 'utf8', function(writeErr) {
-              if (writeErr) return console.log(writeErr);
-            });
-          });
+        fs.writeFile('./dist/index.html', data, 'utf8', function(writeErr) {
+          if (writeErr) return console.log(writeErr);
         });
-      }
-    ]);
+      });
+      fs.mkdir('./dist/ping',function(e){
+        fs.writeFile('./dist/ping/ping', '', 'utf8', function(writeErr) {
+          if (writeErr) return console.log(writeErr);
+        });
+      });
+    });
+  }
+]);
 
 module.exports = config;
