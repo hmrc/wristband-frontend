5949ed84a542697275e3914f03d1c9f4adeadcfa
Tom van Neerijnen <tom@tomvn.com>
2015-08-21 14:23:20 +0100

    redirect logged in users away from login

--- fe/views.py
+++ fe/views.py
@@ -15,6 +15,8 @@ def get_login():
 def do_login():
     # POST to the API/login with the username and password for validation
     # expect back an access token to be stored in a session for use in future API calls
+    if session["fakeauth"]:
+        return redirect(url_for('get_apps'))
     r = requests.post("{}login/".format(current_app.config["API_URI"]), request.form)
     session["username"] = request.form["username"]
     session["fakeauth"] = r.content
