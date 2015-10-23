897387d63faa06e0d31475b6b790d5242c7cfb2a
Tom van Neerijnen <tom@tomvn.com>
2015-10-23 16:09:39 +0100

    ignore spurious 500 errors from wristband on reloads

--- fe/static/js/refresh-table.js
+++ fe/static/js/refresh-table.js
@@ -18,9 +18,16 @@ define(['filter-app-table-by-name'], function (filter_app_table_by_name) {
               $('.label.failed').closest('tr').addClass('error');
             }
           },
-          error: config.error || function () {
-            // if there's an error, you're probably not logged in, so reload
-            window.location.reload();
+          error: config.error || function (error) {
+            if (error.status == 500) {
+                /*  #FIXME 500s are periodically returned when docktor is being deployed or load
+                           balancers are restarted. For now we'll ignore these..
+                */
+                console.log("error -> ", error);
+            } else {
+                // if there's an error that isn't a 500 you're probably not logged in, so reload
+                window.location.reload();
+            }
           },
           complete: function () {
             setTimeout(refreshTable, config.seconds * 1000);
